0b5e891b70cb0e1e59b912ad125e8bb32cc2d53c
Christian Ray Leovido <christian.leovido@gmail.com>
2016-09-14 15:28:41 +0100

    add pokecell file and updated layout

---
+++ pokedex_christian_leovido_swift3/PokeCell.swift
@@ -0,0 +1,9 @@
+//
+//  PokeCell.swift
+//  pokedex_christian_leovido_swift3
+//
+//  Created by Christian Leovido on 14/09/2016.
+//  Copyright © 2016 Christian Leovido. All rights reserved.
+//
+
+import Foundation
