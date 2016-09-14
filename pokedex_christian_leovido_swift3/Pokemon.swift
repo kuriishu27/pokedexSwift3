d0876d05796e3f5c5990d4ae0887c2b74bba31b3
Christian Ray Leovido <christian.leovido@gmail.com>
2016-09-14 15:05:14 +0100

    Added class for pokemon

---
+++ pokedex_christian_leovido_swift3/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  pokedex_christian_leovido_swift3
+//
+//  Created by Christian Leovido on 14/09/2016.
+//  Copyright © 2016 Christian Leovido. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
